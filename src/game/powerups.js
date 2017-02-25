6c3e4b072cbc88ab67e5a19d891bae0924891b3c
do-pe <d.petschenka@syzygy.de>
2017-02-25 16:02:02 +0100

    ideas

--- src/game/powerups.js
+++ src/game/powerups.js
@@ -3,4 +3,9 @@ const powerups = {
   changeSpeed: "CHANGE_SPEED", // player can accelerate/slow down the ball by pressing left/right
   beat: "BEAT_BALL", // earn bonus by pressing a button when the ball hits
   narrow: "NARROW", // panels move closer together - harder for both players
+  fireball: "FIREBALL", // avoid the ball or it destroys the paddle
+  switchSides: "SWITCH_SIDES", // paddles change sides
+  largePaddle: "LARGE_PADDLE",
+  smallPaddle: "SMALL_PADDLE",
+  coop: "COOP", // both players switch to the same side and there are two balls? - maybe not a powerup, but rather a "mode"
 }
